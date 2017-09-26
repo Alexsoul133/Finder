0d704f53eb221531e9b36aeab7fb376be2768a7e
Alexsoul133 <alexsoul133@gmail.com>
2017-09-26 18:52:25 +1000

    sync

--- deck/deck.go
+++ deck/deck.go
@@ -3,7 +3,8 @@ package deck
 import "fmt"
 
 type Deck struct {
-	Deck []string
+	deck       []string
+	head, tail int
 }
 
 func New() *Deck {
@@ -12,42 +13,41 @@ func New() *Deck {
 
 //PushBack value to stock
 func (d *Deck) PushBack(v string) {
-	d.Deck = append(d.Deck, v)
+	d.deck = append(d.deck, v)
 }
 
 //PopBack value from stock
 func (d *Deck) PopBack() (string, error) {
-	if len(d.Deck) == 0 {
+	if len(d.deck) == 0 {
 		return "", fmt.Errorf("nothing to pop")
 	}
-	res := d.Deck[len(d.Deck)-1]
-	d.Deck = d.Deck[:len(d.Deck)-1]
+	res := d.deck[len(d.deck)-1]
+	d.deck = d.deck[:len(d.deck)-1]
 	return res, nil
 }
 
 func (d *Deck) PushFront(v string) {
-	// d.Deck = append(d.Deck, " ")
-	// copy(d.Deck[1:], d.Deck[0:])
-	d.Deck = append([]string{v}, d.Deck...)
+	d.deck = append([]string{v}, d.deck...)
+
 }
 
 func (d *Deck) PopFront() (string, error) {
-	if len(d.Deck) == 0 {
+	if len(d.deck) == 0 {
 		return "", fmt.Errorf("nothing to pop")
 	}
-	res := d.Deck[0]
-	d.Deck = d.Deck[1:]
+	res := d.deck[0]
+	d.deck = d.deck[1:]
 	return res, nil
 }
 
 func (d *Deck) Len() int {
-	return len(d.Deck)
+	return len(d.deck)
 }
 
 func (d *Deck) String() string {
 	str := "\n"
-	for i := range d.Deck {
-		str += fmt.Sprintf("%v %v\n", i, d.Deck[i])
+	for i := range d.deck {
+		str += fmt.Sprintf("%v %v\n", i, d.deck[i])
 	}
 	return str
 }
