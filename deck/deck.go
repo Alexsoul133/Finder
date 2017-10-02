b1fb435d3e2666db50a85f5121873f0e4ec3a07f
Alexsoul133 <alexsoul133@gmail.com>
2017-10-02 14:59:40 +1000

    cycle, reverse

--- deck/deck.go
+++ deck/deck.go
@@ -7,86 +7,90 @@ type Deck struct {
 	head, tail int
 }
 
+var inv = 1
+
 func New() *Deck {
-	return &Deck{deck: make([]string, 4, 4)}
+	return &Deck{deck: make([]string, 4, 4), head: 0, tail: 3}
 }
 
-//PushBack value to stock
-func (d *Deck) PushBack(v string) {
-	// if d.tail < cap(d.deck) {
-	// 	d.deck[d.tail] = v
-	// 	d.tail++
-	// } else if d.head > d.tail && d.tail == cap(d.deck) {
-	// 	d.tail = 0
-	// 	d.deck[d.tail] = v
-	// 	d.tail++
-	// } else {
-	// 	res := make([]string, 2*len(d.deck), 2*cap(d.deck))
-	// 	copy(res, d.deck)
-	// 	d.deck = res
-	// 	d.deck[d.tail] = v
-	// 	d.tail++
-	// }
-
-	if d.tail == d.head && d.tail > 0 {
-		d.tail = cap(d.deck)
-		res := make([]string, 2*len(d.deck), 2*cap(d.deck))
-		copy(res, d.deck)
-		d.deck = res
-	}
-	if d.tail == cap(d.deck) {
-		d.tail = 0
-	}
+func (d *Deck) pushtocap(v string) {
+	d.tail = cap(d.deck)
+	res := make([]string, 2*len(d.deck), 2*cap(d.deck))
+	copy(res, d.deck)
+	d.deck = res
 	d.deck[d.tail] = v
 	d.tail++
-
 }
 
-//PopBack value from stock
+//PushBack value to deck
+func (d *Deck) PushBack(v string) {
+	if d.head-1 == d.tail {
+		inv = 1
+		d.pushtocap(v)
+	} else {
+		if d.tail == cap(d.deck)-1 && d.head == 0 {
+			inv = -1
+		} else if d.tail == cap(d.deck) && d.head > 0 {
+			d.tail = 0
+			inv = 1
+		}
+		d.deck[d.tail] = v
+		d.tail += inv
+	}
+}
+
+//PopBack value from deck
 func (d *Deck) PopBack() (string, error) {
 	if d.head == d.tail {
 		return "", fmt.Errorf("nothing to pop")
 	}
-	res := d.deck[d.tail-1]
-	d.deck[d.tail-1] = ""
-	d.tail--
+	d.tail -= inv
+	if d.tail < 0 {
+		d.tail = cap(d.deck) - 1
+	}
+	res := d.deck[d.tail]
+	d.deck[d.tail] = ""
 	return res, nil
 }
 
 func (d *Deck) PushFront(v string) {
-	if d.head > 0 {
-		d.deck[d.head-1] = v
-		d.head--
-	} else if d.head == 0 && d.tail < cap(d.deck) {
-		d.deck[d.tail] = v
-		d.tail++
-	} else {
-		d.tail += cap(d.deck)
-		d.head += cap(d.deck)
-		res := make([]string, len(d.deck), cap(d.deck))
-		d.deck = append(res, d.deck...)
-		d.deck[d.head-1] = v
-		d.head--
+	if d.head == d.tail {
+		d.pushtocap(v)
+	}
+	d.deck[d.head-1] = v
+	d.head -= inv
+	if d.head < 0 {
+		inv = 1
+	}
+	if d.head == cap(d.deck) {
+		d.head = 0
 	}
 }
 
 func (d *Deck) PopFront() (string, error) {
-	if d.head == cap(d.deck) {
+	if d.head == d.tail || d.deck[d.head] == "" {
 		return "", fmt.Errorf("nothing to pop")
 	}
+
 	res := d.deck[d.head]
 	d.deck[d.head] = ""
-	d.head++
+	if d.head == 0 {
+		inv = 1
+	}
+	d.head += inv
+	if d.head == cap(d.deck) {
+		d.head = 0
+	}
 	return res, nil
 }
 
 func (d *Deck) Len() int {
 	return len(d.deck)
 }
+
 func (d *Deck) Cap() int {
 	return cap(d.deck)
 }
-
 func (d *Deck) String() string {
 	str := "\n"
 	for i := range d.deck {
--- deck/deck_test.go
+++ deck/deck_test.go
@@ -7,37 +7,74 @@ import (
 
 func TestDeck(t *testing.T) {
 	deck := New()
-
+	fmt.Printf("%v H: %v T: %v Cap: %v Len: %v\n", deck, deck.head, deck.tail, deck.Cap(), deck.Len())
 	if v := deck.Len(); v != 4 {
 		t.Errorf("Expected 4, got %v", v)
 		t.Error(deck)
 	}
+	deck.PopFront()
+	fmt.Printf("%v H: %v T: %v Cap: %v Len: %v\n", deck, deck.head, deck.tail, deck.Cap(), deck.Len())
 	deck.PushBack("One")
-	fmt.Printf("%v %v %v Cap: %v Len: %v\n", deck, deck.head, deck.tail, deck.Cap(), deck.Len())
+	fmt.Printf("%v H: %v T: %v Cap: %v Len: %v\n", deck, deck.head, deck.tail, deck.Cap(), deck.Len())
+
+	if v, err := deck.PopFront(); v != "" || err == nil {
+		t.Errorf("Expected \"\",err, got %v %v", v, err)
+		t.Error(deck)
+	}
+	fmt.Printf("%v H: %v T: %v Cap: %v Len: %v\n", deck, deck.head, deck.tail, deck.Cap(), deck.Len())
+	deck.PopFront()
+	fmt.Printf("%v H: %v T: %v Cap: %v Len: %v\n", deck, deck.head, deck.tail, deck.Cap(), deck.Len())
+
 	deck.PushBack("Two")
-	fmt.Printf("%v %v %v Cap: %v Len: %v\n", deck, deck.head, deck.tail, deck.Cap(), deck.Len())
+	fmt.Printf("%v H: %v T: %v Cap: %v Len: %v\n", deck, deck.head, deck.tail, deck.Cap(), deck.Len())
+	deck.PopFront()
+	fmt.Printf("%v H: %v T: %v Cap: %v Len: %v\n", deck, deck.head, deck.tail, deck.Cap(), deck.Len())
 	deck.PushBack("Three")
-	fmt.Printf("%v %v %v Cap: %v Len: %v\n", deck, deck.head, deck.tail, deck.Cap(), deck.Len())
+	fmt.Printf("%v H: %v T: %v Cap: %v Len: %v\n", deck, deck.head, deck.tail, deck.Cap(), deck.Len())
 	deck.PushBack("Four")
-	fmt.Printf("%v %v %v Cap: %v Len: %v\n", deck, deck.head, deck.tail, deck.Cap(), deck.Len())
-	if v := deck.Len(); v != 4 {
-		t.Errorf("Expected 4, got %v", v)
-		t.Error(deck)
-	}
-	if v, err := deck.PopFront(); v != "One" || err != nil {
-		t.Errorf("Expected One,nil, got %v %v", v, err)
-		t.Error(deck)
-	}
-	fmt.Printf("%v %v %v Cap: %v Len: %v\n", deck, deck.head, deck.tail, deck.Cap(), deck.Len())
-	if v, err := deck.PopFront(); v != "Two" || err != nil {
-		t.Errorf("Expected Two,nil, got %v %v", v, err)
-		t.Error(deck)
-	}
-	fmt.Printf("%v %v %v Cap: %v Len: %v\n", deck, deck.head, deck.tail, deck.Cap(), deck.Len())
-	deck.PushBack("Five")
-	fmt.Printf("%v %v %v Cap: %v Len: %v\n", deck, deck.head, deck.tail, deck.Cap(), deck.Len())
+	fmt.Printf("%v H: %v T: %v Cap: %v Len: %v\n", deck, deck.head, deck.tail, deck.Cap(), deck.Len())
+	deck.PopFront()
+	fmt.Printf("%v H: %v T: %v Cap: %v Len: %v\n", deck, deck.head, deck.tail, deck.Cap(), deck.Len())
+	deck.PushFront("Five")
+	fmt.Printf("%v H: %v T: %v Cap: %v Len: %v\n", deck, deck.head, deck.tail, deck.Cap(), deck.Len())
 	deck.PushBack("Six")
-	fmt.Printf("%v %v %v Cap: %v Len: %v\n", deck, deck.head, deck.tail, deck.Cap(), deck.Len())
+	fmt.Printf("%v H: %v T: %v Cap: %v Len: %v\n", deck, deck.head, deck.tail, deck.Cap(), deck.Len())
+	deck.PopBack()
+	fmt.Printf("%v H: %v T: %v Cap: %v Len: %v\n", deck, deck.head, deck.tail, deck.Cap(), deck.Len())
 	deck.PushBack("Seven")
-	fmt.Printf("%v %v %v Cap: %v Len: %v\n", deck, deck.head, deck.tail, deck.Cap(), deck.Len())
+	fmt.Printf("%v H: %v T: %v Cap: %v Len: %v\n", deck, deck.head, deck.tail, deck.Cap(), deck.Len())
+	if v, err := deck.PopBack(); v != "Seven" || err != nil {
+		t.Errorf("Expected \"Seven\",nil, got %v %v", v, err)
+		t.Error(deck)
+	}
+	fmt.Printf("%v H: %v T: %v Cap: %v Len: %v\n", deck, deck.head, deck.tail, deck.Cap(), deck.Len())
+	deck.PushBack("Eight")
+	fmt.Printf("%v H: %v T: %v Cap: %v Len: %v\n", deck, deck.head, deck.tail, deck.Cap(), deck.Len())
+	deck.PopFront()
+	fmt.Printf("%v H: %v T: %v Cap: %v Len: %v\n", deck, deck.head, deck.tail, deck.Cap(), deck.Len())
+	deck.PopFront()
+	fmt.Printf("%v H: %v T: %v Cap: %v Len: %v\n", deck, deck.head, deck.tail, deck.Cap(), deck.Len())
+	deck.PopFront()
+	fmt.Printf("%v H: %v T: %v Cap: %v Len: %v\n", deck, deck.head, deck.tail, deck.Cap(), deck.Len())
+	deck.PushBack("One")
+	fmt.Printf("%v H: %v T: %v Cap: %v Len: %v\n", deck, deck.head, deck.tail, deck.Cap(), deck.Len())
+	deck.PushBack("One")
+	fmt.Printf("%v H: %v T: %v Cap: %v Len: %v\n", deck, deck.head, deck.tail, deck.Cap(), deck.Len())
+	deck.PushBack("One")
+	fmt.Printf("%v H: %v T: %v Cap: %v Len: %v\n", deck, deck.head, deck.tail, deck.Cap(), deck.Len())
+	deck.PushBack("One")
+	fmt.Printf("%v H: %v T: %v Cap: %v Len: %v\n", deck, deck.head, deck.tail, deck.Cap(), deck.Len())
+	deck.PushBack("One")
+	fmt.Printf("%v H: %v T: %v Cap: %v Len: %v\n", deck, deck.head, deck.tail, deck.Cap(), deck.Len())
+	deck.PushFront("One")
+	fmt.Printf("%v H: %v T: %v Cap: %v Len: %v\n", deck, deck.head, deck.tail, deck.Cap(), deck.Len())
+	deck.PushFront("One")
+	fmt.Printf("%v H: %v T: %v Cap: %v Len: %v\n", deck, deck.head, deck.tail, deck.Cap(), deck.Len())
+	deck.PushBack("One")
+	fmt.Printf("%v H: %v T: %v Cap: %v Len: %v\n", deck, deck.head, deck.tail, deck.Cap(), deck.Len())
+	if v, err := deck.PopFront(); v != "One" || err != nil {
+		t.Errorf("Expected \"One\",nil, got %v %v", v, err)
+		t.Error(deck)
+	}
+	fmt.Printf("%v H: %v T: %v Cap: %v Len: %v\n", deck, deck.head, deck.tail, deck.Cap(), deck.Len())
 }
